0e37fdea75d4577d6016ea1b69beac3337a783fe
Bassem <bassempro1@gmail.com>
2020-02-12 11:24:39 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine("Hello World!"+x);
+
+            if (x < 30 ) {
+                Console.WriteLine("X is less than thirty");
+
+            }else if ( x == 30 ){
+                Console.WriteLine("X is thirty");
+
+            }
+            else if ( x > 30 ){
+                Console.WriteLine("X is bigger than thirty");
+
+            }
+        }
+    }
+}
